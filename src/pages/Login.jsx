784006fd1e15aebc61c936d0687971487aa37436
Kleber <kleberst88@gmail.com>
2022-11-25 12:13:19 -0300

    Init project

---
+++ src/pages/Login.jsx
@@ -0,0 +1,45 @@
+import { Button, TextField } from '@mui/material';
+import { Container } from '@mui/system';
+import GoogleIcon from '@mui/icons-material/Google';
+import './login.css';
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container">
+      <Container
+        maxWidth="sm"
+        sx={{
+          marginTop: 30,
+        }}
+      >
+        <h1>MyWallet</h1>
+        <TextField
+          fullWidth
+          label="Login"
+          margin="dense"
+          sx={{ label: { color: '#fff' }, input: { color: '#fff' } }}
+        />
+        <TextField
+          fullWidth
+          label="Senha"
+          margin="dense"
+          sx={{ label: { color: '#fff' }, input: { color: '#fff' } }}
+        />
+        <Button fullWidth variant="contained" sx={{ marginTop: 2 }}>
+          Login with Email
+        </Button>
+        <Button
+          fullWidth
+          variant="contained"
+          endIcon={<GoogleIcon />}
+          sx={{ marginTop: 2 }}
+        >
+          Login With
+        </Button>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
